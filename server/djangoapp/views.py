e03bd15304bab310d3b2be76fcecf3e80677c539
Inti Ortega <69479686+supaycha@users.noreply.github.com>
2024-11-27 16:57:40 -0600

    trying to pass linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,7 +30,9 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+        )
     return JsonResponse({"CarModels": cars})
 
 
@@ -152,6 +154,8 @@ def add_review(request):
             response = post_review(data)
             return JsonResponse({"status": 200})
         except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
